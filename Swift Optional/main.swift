c3ddb19dee05e0d464a106273a2e236fb9f11870
reydaniel <reynaldidaniel@outlook.com>
2021-01-25 21:10:42 +0700

    --Update-- On this learning, I add optional handling for optional using IF statement, Optional Binding, Guard Statement, and Nil-Coalescing Operator.

--- Swift Optional/main.swift
+++ Swift Optional/main.swift
@@ -35,5 +35,70 @@ print(optValue!)
 // However, remember, this kind of unwrapping mechanism should only be used when you are certain
 // that the optional will sure have a value when you access it.
 
+//--Explicitly Declaring an Unwrapped Optional
+let expValue: Int! = 5
+print(expValue)
 
+//--Optional Handling Using IF Statement--
+var nilValue: Int?
+var contValue: Int! = 0
 
+if nilValue != nil {
+    print("It has some value \(nilValue!)")
+} else {
+    print("Doesn't contain value")
+}
+
+if contValue != nil {
+    print("It has some value \(contValue!)")
+} else {
+    print("Doesn't contain value")
+}
+
+//--Optional Binding--
+// Optional binding helps us to find out whether an oprional contains a value or not.
+if let temp = nilValue {
+    print("It has some value \(temp)")
+} else {
+    print("Doesn't contain value")
+}
+
+if let temp = contValue {
+    print("It has some value \(temp)")
+} else {
+    print("Doesn't contain value")
+}
+
+// In the above program, the code inside if statement executes if the optional conatins a value.
+// The let-if statement also automatically unwraps the value and places the unwrapped value in temp constant.
+// This technique has major advantage because it doesn't need to forcely unwrap the value.
+
+//--Guard Statement--
+func testFunction() {
+    let someValue: Int? = 6
+    guard let temp = someValue else {
+        return
+    }
+    print("It has some value \(temp)")
+}
+testFunction()
+
+// In the above program, if it contains a value the guard-let statement automatically unwraps the value and places the unwrapped value in temp constant.
+
+//--Nil-coalescing Operator
+// It is defined as (a ?? b). It unwraps an optional "a" and returns it if it contains a value or returns a default value "b" if "a" is nil.
+var exNil: Int!
+var defaultValue = 8
+let unwrappedValue: Int = exNil ?? defaultValue
+print(unwrappedValue)
+
+// In the above program, variable exNil is defined optional and contains nil value.
+// The nil coalescing operatior fails to unwrap the optional therefore returns defaultValue.
+
+exNil = 10
+defaultValue = 8
+let unwrappedValueAgain: Int = exNil ?? defaultValue
+print(unwrappedValueAgain)
+
+// However, in the above program, optional variable exNil is initialized with value 10.
+// The nil-coalescing operator successfully unwraps the value from exNil.
